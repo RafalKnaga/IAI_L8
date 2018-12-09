d8c2985b07d8bb707207fe90257bbe4876146db7
RafalKnaga <30686@student.pwsztar.edu.p>
2018-12-09 15:05:33 +0100

    components

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { KontaktComponent } from './components/kontakt/kontakt.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    KontaktComponent,
+    MapaComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule
